41b185c448022500eb66d025e146c0b70bbbe1f0
Muralidharan C <murali.c@ensurity.com>
2021-09-22 12:30:07 +0530

    Added V1

--- enscrypt.go
+++ enscrypt.go
@@ -9,34 +9,106 @@ import (
 	"encoding/base64"
 	"fmt"
 	"io"
+	"strconv"
+	"strings"
 
 	"golang.org/x/crypto/pbkdf2"
 )
 
+const (
+	prfSha1   uint = 0
+	prfSha256 uint = 1
+	prfSha512 uint = 2
+)
+
+const (
+	kdfVersion3 byte = 3
+)
+
 // HashPassword ...
 func HashPassword(password string, v byte, prf uint, count uint) string {
-	if v == 3 {
-		return HashPasswordV3(password, prf, count)
-	} else {
+	switch v {
+	case 1:
+		return hashPasswordV1(password, prf, count)
+	case 3:
+		return hashPasswordV3(password, prf, count)
+	default:
 		return ""
 	}
 }
 
 // VerifyPassword ..
 func VerifyPassword(password string, paaswordHash string) bool {
+	if strings.Contains(paaswordHash, "$MYHASH$V1") {
+		return verifyPasswordV1(password, paaswordHash)
+	}
 	pwd, err := base64.StdEncoding.DecodeString(paaswordHash)
 	if err != nil {
 		return false
 	}
 	if pwd[0] == 0x01 {
-		return VerifyPasswordV3(password, pwd)
+		return verifyPasswordV3(password, pwd)
 	} else {
 		return false
 	}
 }
 
 // VerifyPasswordV3 ..
-func VerifyPasswordV3(password string, pwd []byte) bool {
+func verifyPasswordV1(password string, passswordHash string) bool {
+	tempStr := strings.ReplaceAll(passswordHash, "$MYHASH$V1$", "")
+
+	splitStr := strings.Split(tempStr, "$")
+
+	count, err := strconv.ParseInt(splitStr[0], 10, 64)
+
+	if err != nil {
+		return false
+	}
+
+	hashBytes, err := base64.StdEncoding.DecodeString(splitStr[1])
+
+	if err != nil {
+		return false
+	}
+
+	if len(hashBytes) < 16 {
+		return false
+	}
+
+	salt := hashBytes[:16]
+
+	hashSize := len(hashBytes) - 16
+
+	var prf int
+
+	switch hashSize {
+	case 20:
+		prf = 0
+	case 32:
+		prf = 1
+	case 64:
+		prf = 2
+	default:
+		return false
+	}
+
+	var subkey []byte
+	if prf == 0 {
+		subkey = pbkdf2.Key([]byte(password), salt, int(count), 20, sha1.New)
+	} else if prf == 1 {
+		subkey = pbkdf2.Key([]byte(password), salt, int(count), 32, sha256.New)
+	} else {
+		subkey = pbkdf2.Key([]byte(password), salt, int(count), 64, sha512.New)
+	}
+	if bytes.Equal(subkey, hashBytes[16:]) {
+		return true
+	} else {
+		return false
+	}
+}
+
+// VerifyPasswordV3 ..
+func verifyPasswordV3(password string, pwd []byte) bool {
 	prf := ReadNetworkOrder(pwd, 1)
 	count := ReadNetworkOrder(pwd, 5)
 	sl := ReadNetworkOrder(pwd, 9)
@@ -57,8 +129,27 @@ func VerifyPasswordV3(password string, pwd []byte) bool {
 	}
 }
 
-// HashPasswordV3 ..
-func HashPasswordV3(password string, prf uint, count uint) string {
+// hashPasswordV1 ..
+func hashPasswordV1(password string, prf uint, count uint) string {
+	salt := make([]byte, 16)
+	io.ReadFull(rand.Reader, salt[:])
+	var subkey []byte
+	if prf == 0 {
+		subkey = pbkdf2.Key([]byte(password), salt, int(count), 20, sha1.New)
+	} else if prf == 1 {
+		subkey = pbkdf2.Key([]byte(password), salt, int(count), 32, sha256.New)
+	} else {
+		subkey = pbkdf2.Key([]byte(password), salt, int(count), 64, sha512.New)
+	}
+	result := make([]byte, len(salt)+len(subkey))
+	copy(result[0:], salt)
+	copy(result[(len(salt)):], subkey)
+	hashStr := fmt.Sprintf("$MYHASH$V1$%d$%s", count, base64.StdEncoding.EncodeToString(result))
+	return hashStr
+}
+
+// hashPasswordV3 ..
+func hashPasswordV3(password string, prf uint, count uint) string {
 	salt := make([]byte, 16)
 	io.ReadFull(rand.Reader, salt[:])
 	var subkey []byte
